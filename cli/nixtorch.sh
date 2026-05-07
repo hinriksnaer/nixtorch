9fd84be6f63f5b2d0748900f6fd740fe6305512f
hinriksnaer <hgudmund@redhat.com>
2026-05-07 14:32:13 -0400

    Fix gum choose: pre-select all items with --selected='*'

--- cli/nixtorch.sh
+++ cli/nixtorch.sh
@@ -81,7 +81,7 @@ cmd_build() {
   if [[ ${#args[@]} -eq 0 ]]; then
     if has_gum && [[ -t 0 ]]; then
       local selected
-      selected=$(gum choose --no-limit --header "Select projects to build:" $(enabled_projects) </dev/tty) || exit 0
+      selected=$(gum choose --no-limit --selected="*" --header "Select projects to build:" $(enabled_projects)) || exit 0
       if [[ -z "$selected" ]]; then
         info "no projects selected"
         exit 0
